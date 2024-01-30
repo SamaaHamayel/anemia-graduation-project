6d519b3bba8d8c2d397e66b0f254fe14ada7cf2e
pc2023 <hadeere378@gmail.com>
2024-01-30 16:49:18 +0200

    edit app assets

--- lib/core/utils/appImages/images/app_assets.dart
+++ lib/core/utils/appImages/images/app_assets.dart
@@ -1,16 +1,18 @@
 class AppAssets {
   static const String basePath = 'lib/core/utils/appImages/images/';
   static const String splash = '${basePath}splashImage.png';
-  static const String onBoarding0 = '${basePath}onBoardingOne.jpeg';
-  static const String onBoarding1 = '${basePath}onBoardingTwo.jpeg';
-  static const String onBoarding2 = '${basePath}onBoardingThree.jpeg';
-  static const String createNewPassword = '${basePath}create new password.png';
+  static const String onBoarding0 = '${basePath}onBoardingOne.png';
+  static const String onBoarding1 = '${basePath}onBoardingTwo.png';
+  static const String onBoarding2 = '${basePath}onBoardingThree.png';
+  static const String createNewPassword = '${basePath}createNewPassword.png';
   static const String facebook = '${basePath}facebook.png';
   static const String forgetPassword = '${basePath}Forgot password.png';
   static const String google = '${basePath}google.png';
   static const String googlePlus = '${basePath}googlePlus.png';
   static const String twitter = '${basePath}twitter.png';
   static const String verify = '${basePath}verify.png';
-  static const String anemic = '${basePath}you are anemic.png';
-  static const String notAnemic = '${basePath}you arenot anemiatic.png';
+  static const String anemic = '${basePath}anematic.png';
+  static const String notAnemic = '${basePath}notAnematic.png';
+  static const String male = '${basePath}male.png';
+  static const String female = '${basePath}female.png';
 }
