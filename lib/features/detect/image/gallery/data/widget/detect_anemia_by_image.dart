bfb9e4f82322776598050170bb2293afb7e1026d
SamaaHamayel <samaa.hamayel.01@gmail.com>
2024-03-23 07:52:45 +0200

    loading

--- lib/features/detect/image/gallery/data/widget/detect_anemia_by_image.dart
+++ lib/features/detect/image/gallery/data/widget/detect_anemia_by_image.dart
@@ -69,98 +69,101 @@ class DetectAnemiaByImageState extends State<DetectAnemiaByImage> {
     return _pickedImage != null
         ? Image.file(
             _pickedImage!,
-            width: 200,
-            height: 200,
+            
             fit: BoxFit.cover,
           )
-        : Container(
-            width: 203,
-            height: 203,
-            padding: const EdgeInsets.all(30),
-            clipBehavior: Clip.antiAlias,
-            decoration: ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(width: 5, color: Color(0xFFB0B0B0)),
-                borderRadius: BorderRadius.circular(128),
+        : Center(
+          child: Container(
+              width: 203,
+              height: 203,
+              padding: const EdgeInsets.all(30),
+              clipBehavior: Clip.antiAlias,
+              decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 5, color: Color(0xFFB0B0B0)),
+                  borderRadius: BorderRadius.circular(128),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 64,
+                    height: 64,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 56.01,
+                          height: 48,
+                          child: Stack(
+                            children: [
+                              Image.asset('lib/core/utils/appImages/images/uploadimage.png'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 17),
+                  Text(
+                    'Drag & drop  or',
+                    style: TextStyle(
+                      color: Color(0xFF535354),
+                      fontSize: 16,
+                      fontFamily: 'Kodchasan',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                    ),
+                  ),
+                ],
               ),
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 64,
-                  height: 64,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 56.01,
-                        height: 48,
-                        child: Stack(
-                          children: [
-                            Image.asset('lib/core/utils/appImages/images/uploadimage.png'),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 17),
-                Text(
-                  'Drag & drop  or',
-                  style: TextStyle(
-                    color: Color(0xFF535354),
-                    fontSize: 16,
-                    fontFamily: 'Kodchasan',
-                    fontWeight: FontWeight.w700,
-                    height: 0,
-                  ),
-                ),
-              ],
-            ),
-          );
+        );
   }
 
   Widget buildChooseFileButton() {
-    return ElevatedButton(
-      onPressed: pickImageFromGallery,
-      child: Container(
-        width: 197,
-        height: 50,
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-        clipBehavior: Clip.antiAlias,
-        decoration: ShapeDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(0.00, -1.00),
-            end: Alignment(0, 1),
-            colors: [Color(0xFF2BB598), Color(0xFF398272)],
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Choose File',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontFamily: 'Kodchasan',
-                fontWeight: FontWeight.w700,
-                height: 0,
-              ),
+    return Center(
+      child: ElevatedButton(
+        onPressed: pickImageFromGallery,
+        child: Container(
+          width: 197,
+          height: 50,
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+          clipBehavior: Clip.antiAlias,
+          decoration: ShapeDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(0.00, -1.00),
+              end: Alignment(0, 1),
+              colors: [Color(0xFF2BB598), Color(0xFF398272)],
             ),
-          ],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Choose File',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'Kodchasan',
+                  fontWeight: FontWeight.w700,
+                  height: 0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
