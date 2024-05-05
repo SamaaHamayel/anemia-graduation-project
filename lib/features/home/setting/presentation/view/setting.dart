8283a318e968042cd11c1ff3f241c8d3fb3c61bd
SamaaHamayel <samaa.hamayel.01@gmail.com>
2024-05-05 03:47:15 +0300

    another حبة تحابيش

--- lib/features/home/setting/presentation/view/setting.dart
+++ lib/features/home/setting/presentation/view/setting.dart
@@ -15,141 +15,150 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<SettingsCubit, SettingsState>(
-        listener: (context, state) {
-          // TODO: implement listener
-        },
-        builder: (context, state) {
-          return SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.account,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: AppColors.lightPrimaryColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: "Kodchasan"),
-                  ),
-                  SizedBox(
-                    height: 16.h,
-                  ),
-                  Container(
-                    height: 80.h,
-                    width: 343.w,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16.h),
-                      border: Border.all(color: AppColors.grayColor),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/core/utils/appImages/images/background.png'),
+            fit: BoxFit.cover, // Optional: You can set the image fit as needed
+          ),
+        ),
+        child: BlocConsumer<SettingsCubit, SettingsState>(
+          listener: (context, state) {
+            // TODO: implement listener
+          },
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.account,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: AppColors.lightPrimaryColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Kodchasan"),
                     ),
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 16, top: 20, bottom: 10),
-                      child: Row(
-                        children: [
-                          const CircleAvatar(
-                            backgroundImage: AssetImage(AppAssets.female),
-                            radius: 25,
-                          ),
-                          SizedBox(
-                            width: 25.w,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                "Sara Shebl",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600),
-                              ),
-                              const Text("personal info")
-                            ],
-                          ),
-                          const Spacer(),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.arrow_forward_ios,
-                              color: AppColors.lightPrimaryColor,
-                              size: 20,
+                    SizedBox(
+                      height: 16.h,
+                    ),
+                    Container(
+                      height: 80.h,
+                      width: 343.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16.h),
+                        border: Border.all(color: AppColors.grayColor),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 16, top: 20, bottom: 10),
+                        child: Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundImage: AssetImage(AppAssets.female),
+                              radius: 25,
                             ),
-                          )
-                        ],
+                            SizedBox(
+                              width: 25.w,
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "Sara Shebl",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .copyWith(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600),
+                                ),
+                                const Text("personal info")
+                              ],
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.arrow_forward_ios,
+                                color: AppColors.lightPrimaryColor,
+                                size: 20,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 35.h,
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.settings,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: AppColors.lightPrimaryColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: "Kodchasan"),
-                  ),
-                  SizedBox(
-                    height: 15.h,
-                  ),
-                  SettingSwitchCard(
-                    switchValue:
-                        BlocProvider.of<SettingsCubit>(context).isArabicEnable,
-                    onChange: (value) {
-                      BlocProvider.of<SettingsCubit>(context)
-                          .changeCurrentLanguage(value);
-                    },
-                    icon: Icons.language_outlined,
-                    text: AppLocalizations.of(context)!.language,
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                  SettingSwitchCard(
-                    onChange: (value) {
-                      BlocProvider.of<SettingsCubit>(context)
-                          .changeNotification(value);
-                    },
-                    switchValue: BlocProvider.of<SettingsCubit>(context)
-                        .isNotificationEnable,
-                    icon: Icons.notifications_active_outlined,
-                    text: AppLocalizations.of(context)!.notifications,
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                  SettingSwitchCard(
-                    onChange: (value) {
-                      BlocProvider.of<SettingsCubit>(context).changeThem(value);
-                    },
-                    switchValue: BlocProvider.of<SettingsCubit>(context)
-                        .isDarkThemEnable,
-                    icon: Icons.dark_mode_outlined,
-                    text: AppLocalizations.of(context)!.darkMode,
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                  SettingSwitchCard(
-                    onChange: (value) {
-                      BlocProvider.of<SettingsCubit>(context)
-                          .changeLocation(value);
-                    },
-                    switchValue: BlocProvider.of<SettingsCubit>(context)
-                        .isLocationEnable,
-                    icon: Icons.location_on_outlined,
-                    text: AppLocalizations.of(context)!.country,
-                  ),
-                ],
+                    SizedBox(
+                      height: 35.h,
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.settings,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: AppColors.lightPrimaryColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Kodchasan"),
+                    ),
+                    SizedBox(
+                      height: 15.h,
+                    ),
+                    SettingSwitchCard(
+                      switchValue: BlocProvider.of<SettingsCubit>(context)
+                          .isArabicEnable,
+                      onChange: (value) {
+                        BlocProvider.of<SettingsCubit>(context)
+                            .changeCurrentLanguage(value);
+                      },
+                      icon: Icons.language_outlined,
+                      text: AppLocalizations.of(context)!.language,
+                    ),
+                    SizedBox(
+                      height: 25.h,
+                    ),
+                    SettingSwitchCard(
+                      onChange: (value) {
+                        BlocProvider.of<SettingsCubit>(context)
+                            .changeNotification(value);
+                      },
+                      switchValue: BlocProvider.of<SettingsCubit>(context)
+                          .isNotificationEnable,
+                      icon: Icons.notifications_active_outlined,
+                      text: AppLocalizations.of(context)!.notifications,
+                    ),
+                    SizedBox(
+                      height: 25.h,
+                    ),
+                    SettingSwitchCard(
+                      onChange: (value) {
+                        BlocProvider.of<SettingsCubit>(context)
+                            .changeThem(value);
+                      },
+                      switchValue: BlocProvider.of<SettingsCubit>(context)
+                          .isDarkThemEnable,
+                      icon: Icons.dark_mode_outlined,
+                      text: AppLocalizations.of(context)!.darkMode,
+                    ),
+                    SizedBox(
+                      height: 25.h,
+                    ),
+                    SettingSwitchCard(
+                      onChange: (value) {
+                        BlocProvider.of<SettingsCubit>(context)
+                            .changeLocation(value);
+                      },
+                      switchValue: BlocProvider.of<SettingsCubit>(context)
+                          .isLocationEnable,
+                      icon: Icons.location_on_outlined,
+                      text: AppLocalizations.of(context)!.country,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
